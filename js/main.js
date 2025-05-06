c2ab8f41a92b8a4121b8b8e1a817af6b0356b093
Shohanur-Shohan <shohanurshohan734@gmail.com>
2025-05-06 21:05:49 +0600

    added counter at loader

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-gsap.from(".home-text",{
+gsap.from("#home-text",{
     y: 150,
     stagger: 0.25,
     duration: 0.6,
     delay: 0.5
 })
+//text reveal from bottom in loader
+
+//counter in loader
+const heroCounter = document.querySelector("#hero-counter");
+let heroCount = 0;
+
+const interval = setInterval(function(){
+    if(heroCount <= 100){
+       heroCounter.innerHTML = heroCount++;
+    }
+    else {
+        clearInterval(interval); // থামিয়ে দেওয়া হবে যখন শেষ হবে
+    }
+}, 16);
