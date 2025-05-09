4e184cd3bd2d7537b6441080bc17580419141597
Shohanur-Shohan <shohanurshohan734@gmail.com>
2025-05-09 20:23:26 +0600

    responsive header

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,22 @@ module.exports = {
         LatoLight: ['LatoLight', 'sans-serif'],
         LatoRegular: ['LatoRegular', 'sans-serif'],
       },
-      
+      screens: {
+        'xs': '320px',
+        // => @media (min-width: 320px) { ... }
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      }
     },
   },
   plugins: [],
