3058dd175c037a7c513a7a865dc88a2dca70945b
Shohanur-Shohan <shohanurshohan734@gmail.com>
2025-05-20 23:00:06 +0600

    some problem in shery or in loader

--- js/main.js
+++ js/main.js
@@ -51,10 +51,6 @@ document.addEventListener("mousemove", (dets)=>{
     })
 })
 
-// animated magnet at navbar
-Shery.makeMagnet(".magnet-target" /* Element to target.*/, {
-    //Parameters are optional.
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
+
+
 
